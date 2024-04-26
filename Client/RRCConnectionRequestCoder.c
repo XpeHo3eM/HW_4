ba0436382ee2c513b7fb267dfe827fe6cae445d7
XpeHo3eM <npv90@mail.ru>
2024-04-26 15:09:59 +0500

    Added RRCConnectionRequestSetupComplete

--- Client/RRCConnectionRequestCoder.c
+++ Client/RRCConnectionRequestCoder.c
@@ -5,16 +5,11 @@
 #include <netinet/in.h>
 #include <netinet/sctp.h>
 #include <stdio.h>
-#include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #include "RRCConnectionRequestCoder.h"
 
-#define BUFFER_SIZE 1024
-
-#define asn1cSeqAdd(VaR, PtR) \
-    if (asn_sequence_add(VaR, PtR) != 0) assert(0)
 
 void RRCConnectionRequestCoder(uint8_t **buffer, ssize_t *len){
     RRCConnectionRequest_t *request;
@@ -60,45 +55,4 @@ void RRCConnectionRequestCoder(uint8_t **buffer, ssize_t *len){
     }
 
     xer_fprint(stdout, &asn_DEF_RRCConnectionRequest, request);    
-}
-
-
-void tx_send(uint8_t **buffer, ssize_t *len) {
-    struct sockaddr_in servaddr = {
-        servaddr.sin_family = AF_INET,
-        servaddr.sin_port = htons(2500),
-        servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"),
-    };
-
-    int sockfd;
-
-    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
-    if (sockfd < 0) {
-        printf("Error when opening socket\n");
-        exit(1);
-    }
-
-    int ret = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-    if (ret < 0) {
-        printf("Error when connecting socket\n");
-        exit(1);
-    }
-
-    ret = sctp_sendmsg(sockfd, *buffer, *len, NULL, 0, 0, 0, 0, 0, 0);
-    if (ret < 0) {
-        printf("Error when sending msg\n");
-        exit(1);
-    }
-
-    char resp[BUFFER_SIZE];
-    ssize_t valread = read(sockfd, buffer, BUFFER_SIZE);
-    if (valread > 0) {
-        printf("Response from server: %s\n", *buffer);
-    } else {
-        printf("No response received from server.\n");
-    }
-
-    printf("Sent packet\n");
-
-    close(sockfd);
 }
--- Client/build.sh
+++ Client/build.sh
@@ -5,5 +5,5 @@ mkdir build
 
 sudo asn1c lte-rrc-15.6.0.asn1 -D src -pdu=auto -fcompound-names -no-gen-OER -no-gen-example -fno-include-deps
 
-gcc -g -Isrc -I. -o build/rrcConnectionRequest main.c RRCConnectionRequestCoder.c src/*.c -lsctp -DPDU=auto -DASN_DISABLE_OER_SUPPORT
-./build/rrcConnectionRequest
+gcc -g -Isrc -I. -o build/RRC main.c RRCConnectionRequestCoder.c RRCConnectionSetupCompleteCoder.c src/*.c -lsctp -DPDU=auto -DASN_DISABLE_OER_SUPPORT
+./build/RRC
